282d8e12474301f8be4fbffcf3574fe5433830e1
Paulo Sergio <cloverfieldor@gmail.com>
2024-08-28 14:53:19 -0300

    feat: modeling around instagram groups

--- src/Grupo.java
+++ src/Grupo.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
 public class Grupo {
-    ArrayList<Usuario> integrantes;
+    ArrayList<Usuario> members;
+    ArrayList<Message> messages;
     //Usuario vai ser a classe criada
     public void putUser(Usuario novo_integrante) {
         integrantes.add(novo_integrante);
@@ -13,9 +14,16 @@ public class Grupo {
         return this.integrantes;
     }
     public Usuario getUser(int index) {
-        return this.integrantes.get(index)
+        return this.integrantes.get(index);
     }
     public Usuario getUser(Usuario usuario) {
         return this.integrantes.get(this.integrantes.indexOf(usuario));
+    }
+    public void sendMessage(Usuario user,String message) {
+        Message msg = new Message(message, user);
+        messages.add(msg);
+    }
+    public ArrayList<Message> getMessages() {
+        return this.messages;
     }
 }
