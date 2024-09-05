4cc64f41e87c1deefb1e926f03a6b180e5f314ab
Miguel de Campos Rodrigues Moret <miguel.moret@unesp.br>
2024-09-05 18:36:22 -0300

    feat: added a method to remove images using their urls

--- src/PhotoPost.java
+++ src/PhotoPost.java
@@ -3,32 +3,39 @@ import java.util.ArrayList;
 
 
 public class PhotoPost extends Post {
-    private ArrayList<String> images_url;
+    private ArrayList<String> urls;
 
-    public PhotoPost(Usuario postUser, String postText, LocalDateTime date, String content, ArrayList<String> images_url, String username) {
+    public PhotoPost(Usuario postUser, String postText, LocalDateTime date, String content, ArrayList<String> urls, String username) {
         super(postUser, postText);
-        this.images_url = images_url;
+        this.urls = urls;
     }
 
-    public ArrayList<String> getImages_url() { return this.images_url; }
-    public void setImage(ArrayList<String> url_images) { this.images_url = url_images; }
-
-    public void addImage(String new_url) {
-        images_url.add(new_url);
-    }
-
-    public void removeImageByIndex(int index) {
-        if (index >= 0 && index < images_url.size()) {
-            images_url.remove(index);
-        }
-    }
+    public ArrayList<String> getUrls() { return this.urls; }
+    public void setImage(ArrayList<String> url_images) { this.urls = url_images; }
 
     public int getImageIndex(String url) {
-        for (int i = 0; i < images_url.size(); i++) {
-            if (url.equals(images_url.get(i))) {
+        for (int i = 0; i < urls.size(); i++) {
+            if (url.equals(urls.get(i))) {
                 return i;
             }
         }
         return -1;
+    }
+
+    public void addImage(String new_url) {
+        urls.add(new_url);
+    }
+
+    public boolean removeImageByIndex(int index) {
+        if (index >= 0 && index < urls.size()) {
+            urls.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeImageByUrl(String url) {
+        int index = this.getImageIndex(url);
+        return this.removeImageByIndex(index);
     }
 }
