1336a1df27d79fd337aaacf0c7e4d159371bbf3b
Miguel de Campos Rodrigues Moret <miguel.moret@unesp.br>
2024-09-10 20:31:48 -0300

    fix: TextPost and PhotoPost now herdam de PostWithCommentSection instead of Post

--- src/TextPost.java
+++ src/TextPost.java
@@ -1,6 +1,5 @@
-public class TextPost extends Post {
-    // until changes in Postwithcommentsection is made it will come from post.
-    public TextPost(Usuario postUser, String postText) {
+public class TextPost extends PostWithCommentSection {
+    public TextPost(User postUser, String postText) {
         super(postUser, postText);
     }
 }
