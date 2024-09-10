a3db06fb3f012a1ec061c9d611e905454cc86b12
matheusvirissimo <matheus.virissimo@unesp.br>
2024-09-10 01:09:10 -0300

    fix: remaking the User class logic

--- src/User.java
+++ src/User.java
@@ -1,130 +1,143 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class User {
+    private UUID id;
     private String userName;
     private String nickName;
     private String email;
     private String pronouns;
-    
     private String profileDescription;
-    // private profilePicture;
-    
-    private int followers;
-    private int following;
-    
     private boolean accountPrivacy; 
     private LocalDateTime creationDate;
-    
-    // private Post post;
-    private int postsQuantity;
-    private ArrayList<User> friends = new ArrayList<User>();
-    
-    public User(String userName, String nickName, String email, String pronouns, String profileDescription, int followers, int following, boolean accountPrivacy, LocalDateTime creationDate, int postsQuantity){
-        setUserName(userName);
-        setNickName(nickName);
-        setEmail(email);
-        setPronouns(pronouns);
-        
-        setProfileDescription(profileDescription);
-        
-        setFollowers(followers);
-        setFollowing(following);
-        
-        setAccountPrivacy(accountPrivacy);
-        setCreationDate(creationDate);
-        
-        setPostsQuantity(postsQuantity);
+
+    private ArrayList<User> followers; 
+    private ArrayList<User> following;
+    private Set<User> blockedUsers; // evita duplicatas e é mais eficiente nesse caso
+    private ArrayList<Post> userPost; 
+
+
+    public User(UUID id, String userName, String nickName, String email, String pronouns, String profileDescription, boolean accountPrivacy, LocalDateTime creationDate, ArrayList<User> followers, ArrayList<User> following, Set<User> blockedUsers, ArrayList<Post> userPost) {
+        this.id = UUID.randomUUID();
+        this.userName = userName;
+        this.nickName = nickName;
+        this.email = email;
+        this.pronouns = pronouns;
+        this.profileDescription = profileDescription;
+        this.accountPrivacy = accountPrivacy;
+        this.creationDate = creationDate;
+
+        this.followers = new ArrayList<>();
+        this.following = new ArrayList<>();
+        this.blockedUsers = new HashSet<>();
+        this.userPost = new ArrayList<>();
     }
+    
+    
     
     // Getters e Setters
-    
-    public void setUserName(String userName){
-        this.userName = userName;
-    }
-    
-    public String getUserName(){
-        return userName;
-    }
-    
-    public void setNickName(String nickName){
-        this.nickName = nickName;
-    }
-    
-    public String getNickName(){
-        return userName;
-    }
-    
-    public void setEmail(String email){
-        this.email = email; 
-    }
-    
-    public String getEmail(){
-        return email;
-    }
-    
-    public void setPronouns(String pronouns){
-        this.pronouns = pronouns;
-    }
-    
-    public String getPronouns(){
-        return pronouns;
-    }
-    
-    public void setProfileDescription(String profileDescription){
-        this.profileDescription = profileDescription;
-    }
-    
-    public String getProfileDescription(){
-        return profileDescription;
-    }
-    
-    public void setFollowers(int followers){
-        this.followers = followers;
-    }
-    
-    public int getFollowers(){
-        return followers;
-    }
-    
-    public void setFollowing(int following){
-        this.following = following;
-    }
-    
-    public int getFollowing(){
-        return following;
-    }
-    
-    public void setAccountPrivacy(boolean accountPrivacy){
-        this.accountPrivacy = accountPrivacy;
-    }
-    
-    public boolean getAccountPrivacy(){
-        return accountPrivacy;
-    }
-    
-    public void setCreationDate(LocalDateTime creationDate){
-        this.creationDate = creationDate;
-    }
-    
-    public LocalDateTime getCreationDate(){
-        return creationDate;
-    }
-    
-    public void setPostsQuantity(int postsQuantity){
-        this.postsQuantity = postsQuantity;
-    }
-    
-    public int getPostsQuantity(){
-        return postsQuantity;
-    }
-    
-    public void setFriends(ArrayList<User> friends) {
-        this.friends = friends;
-    }
-    
-    public ArrayList<User> getFriends() {
-        return friends;
+
+    public UUID getId() {
+        return id;
     }
 
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPronouns() {
+        return pronouns;
+    }
+
+    public void setPronouns(String pronouns) {
+        this.pronouns = pronouns;
+    }
+
+    public String getProfileDescription() {
+        return profileDescription;
+    }
+
+    public void setProfileDescription(String profileDescription) {
+        this.profileDescription = profileDescription;
+    }
+
+    public boolean isAccountPrivacy() {
+        return accountPrivacy;
+    }
+
+    public void setAccountPrivacy(boolean accountPrivacy) {
+        this.accountPrivacy = accountPrivacy;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public ArrayList<User> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(ArrayList<User> followers) {
+        this.followers = followers;
+    }
+
+    public ArrayList<User> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(ArrayList<User> following) {
+        this.following = following;
+    }
+
+    public Set<User> getBlockedUsers() {
+        return blockedUsers;
+    }
+
+    public void setBlockedUsers(Set<User> blockedUsers) {
+        this.blockedUsers = blockedUsers;
+    }
+
+    public ArrayList<Post> getUserPost() {
+        return userPost;
+    }
+
+    public void setUserPost(ArrayList<Post> userPost) {
+        this.userPost = userPost;
+    }
+    
+    
+    
 }
