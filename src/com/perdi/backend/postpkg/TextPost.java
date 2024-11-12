93351fdc338fbe3a3f6d8d2f98f9bea36b7a3c67
Abigail Sayury <abigailsayury.nakashima@gmail.com>
2024-11-11 23:15:01 -0300

    feat: TextPost changed to new Post, example

--- src/com/perdi/backend/postpkg/TextPost.java
+++ src/com/perdi/backend/postpkg/TextPost.java
@@ -1,7 +1,37 @@
 package com.perdi.backend.postpkg;
 
-public class TextPost extends PostWithCommentSection {
-    public TextPost(User postUser, String postText) {
-        super(postUser, postText);
+import com.perdi.backend.userpkg.User;
+
+public class TextPost extends Post {
+    private String postTextContent;
+
+    public TextPost(User postUser, String postTextContent) {
+        super(postUser);
+        setPostTextContent(postTextContent);
     }
+
+    @Override
+    public Object getContent() {
+        return postTextContent;
+    }
+
+    @Override
+    public void setContent(Object content) {
+        if (content instanceof String string) {
+            setPostTextContent(string);
+            markAsEdited();
+        } else {
+            System.out.println("Conteúdo Inválido");
+        }
+    }
+
+    public String getPostTextContent() {
+        return postTextContent;
+    }
+
+    private void setPostTextContent(String postTextContent) {
+        this.postTextContent = postTextContent;
+    }
+
+
 }
