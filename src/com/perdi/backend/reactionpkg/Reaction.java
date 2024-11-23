020c61d3ef30638fce5de747d1b6379569713ab9
Abigail Sayury <abigailsayury.nakashima@gmail.com>
2024-11-23 14:22:38 -0300

    feat: reaction changes

--- src/com/perdi/backend/reactionpkg/Reaction.java
+++ src/com/perdi/backend/reactionpkg/Reaction.java
@@ -1,8 +1,33 @@
 package com.perdi.backend.reactionpkg;
 
-public enum Reaction {
-    LIKE,
-    DISLIKE,
-    LOVED,
-    HATED
+/**
+ * @author arthu
+ */
+
+import com.perdi.backend.userpkg.User;
+
+public class Reaction {
+    private User user;
+    private ReactionEnum reaction;
+
+    Reaction(User user, ReactionEnum reaction){
+        this.user = user;
+        this.reaction = reaction;
+    }
+
+    public void setReaction(ReactionEnum reaction)
+    {
+        this.reaction = reaction;
+    }
+
+    public ReactionEnum getReaction()
+    {
+        return this.reaction;
+    }
+
+    public User getUser()
+    {
+        return this.user;
+    }
+
 }
