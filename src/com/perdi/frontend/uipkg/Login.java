54673e338a169214b1a18435190d10d2c9ac5e7a
Abigail Sayury <abigailsayury.nakashima@gmail.com>
2024-11-25 18:37:31 -0300

    feat: gui works

--- src/com/perdi/frontend/uipkg/Login.java
+++ src/com/perdi/frontend/uipkg/Login.java
@@ -1,6 +1,9 @@
 package com.perdi.frontend.uipkg;
 
+import com.perdi.backend.storage.datapkg.DataCenter;
+import com.perdi.backend.storage.datapkg.UserPersistence;
 import com.perdi.backend.userpkg.User;
+import com.sun.source.tree.UsesTree;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Login {
@@ -17,15 +21,14 @@ public class Login {
     private JLabel EmailLabel;
     private JTextField EmailTextField;
     private JLabel PasswordLabel;
-    private JPasswordField PasswordField;
     private JButton RegisterButton;
     private JButton LoginButton;
     private JPanel PhotoPanel;
     private JLabel WelcomeLabel;
     private JTextField UserNameField;
     private JLabel UserNameLabel;
-    private JPanel WarningPanel;
     private JTextField WarningField;
+    private JTextField PasswordField;
 
     public Login() {
         LoginButton.addActionListener(new ActionListener() {
@@ -45,23 +48,27 @@ public class Login {
                 }
                 String email = EmailTextField.getText();
                 String userName = UserNameField.getText();
-                String password = Arrays.toString(PasswordField.getPassword());
+                String password = PasswordField.getText();
 
                 if (email.contains("@") && !userName.isEmpty() && !password.isEmpty()) {
                     User newUser = new User(userName, userName, email, null, null, false);
+                    UserPersistence userPersistence = UserPersistence.getInstance();
+                    userPersistence.addUser(newUser);
+                    System.out.println(newUser.getUserName());
+                    System.out.println(newUser.getEmail());
                 } else {
+                    WarningField.setVisible(true);
                     if (!email.contains("@")) {
+                        System.out.println("Invalid Email");
                         WarningField.setText("Invalid email");
-                        WarningField.setVisible(true);
                     }
                     if (userName.isEmpty()) {
-                        System.out.println(true);
+                        System.out.println("Missing UserName");
                         WarningField.setText("Missing UserName");
-                        WarningField.setVisible(true);
                     }
                     if (password.isEmpty()) {
+                        System.out.println("Missing Password");
                         WarningField.setText("Missing Password");
-                        WarningField.setVisible(true);
                     }
                 }
             }
@@ -83,6 +90,7 @@ public class Login {
     }
 
     public static void main(String[] args) {
+        UserPersistence userPersistence = UserPersistence.getInstance();
         JFrame frame = new JFrame("Perdi Social");
         frame.setContentPane(new Login().LoginPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
