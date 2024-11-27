cc9afc3b7ebc80e559d85179400387616b12615c
matheusvirissimo <matheus.virissimo@unesp.br>
2024-11-27 02:33:19 -0300

    refactor: refactor the User class to accept the follow manager

--- src/com/perdi/backend/userpkg/User.java
+++ src/com/perdi/backend/userpkg/User.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 
 import com.perdi.backend.storage.datapkg.DataCenter;
 import com.perdi.backend.feed.postpkg.Post;
-import com.perdi.backend.feed.postpkg.TextPost;
+import com.perdi.backend.feed.postpkg.TextPost;;
 
 public class User {
     private UUID id;
@@ -20,14 +20,17 @@ public class User {
     private Boolean accountPrivacy; 
     private LocalDateTime creationDate;
 
+
     private ArrayList<User> followers; 
     private ArrayList<User> following;
+    private FollowManager followManager = new FollowersFollowing();
+    
     private Set<User> blockedUsers; // evita duplicatas e é mais eficiente nesse caso
     private ArrayList<Post> userPost;
 
     private static DataCenter dataCenter = DataCenter.getInstance();
 
-    public User(String userName, String nickName, String email, String pronouns, String profileDescription, Boolean accountPrivacy) {
+    protected User(String userName, String nickName, String email, String pronouns, String profileDescription, Boolean accountPrivacy) {
         this.id = UUID.randomUUID();
         this.userName = userName;
         this.nickName = nickName;
@@ -37,8 +40,7 @@ public class User {
         this.accountPrivacy = accountPrivacy;
         this.creationDate = LocalDateTime.now();
 
-        this.followers = new ArrayList<>();
-        this.following = new ArrayList<>();
+        
         this.blockedUsers = new HashSet<>();
         this.userPost = new ArrayList<>();
 
@@ -46,127 +48,27 @@ public class User {
     }
     
     
-    // Editar perfil
-    public void editProfile(String userName, String nickName, String email, String pronouns, String profileDescription, Boolean accountPrivacy){
-        // se o parâmetro for nulo, significa que o usuário não quer alterar esse parâmetro
-        if(userName != null){
-            setUserName(userName);
-        }
-
-        if(nickName != null){
-            setNickName(nickName);
-        }
-
-        if(email != null){
-            setEmail(email);
-        }
-
-        if(pronouns != null){
-            setPronouns(pronouns);
-        }
-
-        if(profileDescription != null){
-            setProfileDescription(profileDescription);
-        }
-
-        if(accountPrivacy != null){
-            setAccountPrivacy(accountPrivacy);
-        }
+     // Métodos delegados
+    public boolean follow(User user) {
+        return followManager.follow(this, user);
     }
 
-    // Possibilidade de seguir um novo usuário
-    public void followUser(User user){
-        if(user == null || blockedUsers.contains(user)){ // usuário não existe ou está bloqueado
-            return;
-        }
-
-        // função inspirada na do Arthur + Miguel
-        if(!this.equals(user)){ // evita que o usuário siga a si mesmo
-            this.following.add(user);
-            user.followers.add(this);
-        }
+    public boolean unfollow(User user) {
+        return followManager.unfollow(this, user);
     }
 
-    public void removeFollower(User user){
-        if(user == null){
-            return;
-        }
-
-        if(followers.contains(user)){
-            followers.remove(user);
-            user.following.remove(this);
-        }
-
-        return;
+    public boolean removeFollower(User user) {
+        return followManager.removeFollower(this, user);
     }
 
-    public int followersQuantity(){
-        return followers.size();
+    public int getFollowersCount() {
+        return followManager.getFollowersCount(this);
     }
 
-    public int followingQuanitty(){
-        return following.size();
+    public int getFollowingCount() {
+        return followManager.getFollowingCount(this);
     }
-
-    // Bloquear outro usuário
-    public void blockUser(User user){
-        if(user == null){
-            return;
-        }
-
-        blockedUsers.add(user);
-        following.remove(user);
-        followers.remove(user);
-
-        // o this se refere ao próprio objeto (isabella help)
-        user.followers.remove(this);
-        user.following.remove(this);
-    }
-
-    public void unblockUser(User user){
-        if(user == null){ 
-            return; 
-        }
-
-        if(!blockedUsers.contains(user)){ // ()== false) usuário não está bloqueado
-            return;
-        }
-
-        blockedUsers.remove(user);
-
-        // não necessariamente o usuário quer seguir e ser seguido
-        return; 
-    }
-
-    // Adicionar post
-    public void addTextPost(String postText){
-        Post newPost = new TextPost(this.id, postText);
-        userPost.add(newPost);
-
-        return;
-    }
-
-    // Remover post
-    public void removePost(UUID postID){ // procura o post pelo seu ID
-
-        Post postToRemove = null;
-
-        // vasculhar cada um dos posts procurando pelo ID
-        for(int i = 0; i < userPost.size(); i++){ 
-            if(userPost.get(i).getPostID().equals(postID)){ // encontrou o post
-                postToRemove = userPost.get(i); // posição do post
-                break; // não precisa continuar a iteração 
-            }
-        }
-
-        if(postToRemove == null){ // não encontrou
-            return;
-        }else{
-            userPost.remove(postToRemove); // remove o post
-            dataCenter.removePost(this.id,postID);
-            return;
-        }
-    }
+   
     
     // Getters e Setters
     public UUID getId() {
@@ -263,18 +165,6 @@ public class User {
 
     public void setUserPost(ArrayList<Post> userPost) {
         this.userPost = userPost;
-    }
-    
-    public Boolean isFollowing(UUID followedID)
-    {
-        for(User user: following)
-        {
-            if(user.getId().equals(followedID))
-            {
-                return true;
-            }
-        }
-        return false;
     }
     
 }
